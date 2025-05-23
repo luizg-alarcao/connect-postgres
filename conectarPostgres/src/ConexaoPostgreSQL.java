9f1dc0318d4d67aac394a4ec1aec8ab856d9cbc9
Luiz Gustavo Alarcão <lgca15670@gmail.com>
2025-05-23 10:14:02 -0300

    Update ConexaoPostgreSQL.java

--- conectarPostgres/src/ConexaoPostgreSQL.java
+++ conectarPostgres/src/ConexaoPostgreSQL.java
@@ -4,16 +4,16 @@ import java.sql.SQLException;
 
 public class ConexaoPostgreSQL {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/JDBC"; // nome do seu banco
-        String usuario = "postgres"; // seu usuário
-        String senha = "1"; // sua senha
+        String url = "jdbc:postgresql://localhost:5432/JDBC";
+        String usuario = "postgres";
+        String senha = "1";
 
         try {
             Connection conexao = DriverManager.getConnection(url, usuario, senha);
-            System.out.println("Conexão bem-sucedida!");
+            System.out.println("Banco de dados conectado com sucesso!");
             conexao.close();
         } catch (SQLException e) {
-            System.out.println("Erro na conexão:");
+            System.out.println("Não foi possivel conectar ao Banco de Dados, Erro: ");
             e.printStackTrace();
         }
     }
